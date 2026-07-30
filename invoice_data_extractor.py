ed6b044630d4e6f2dcc11cada976f50217bd90dc
blindcamel <steve@blindcamel.com>
2026-07-30 07:54:28 -0700

    swapped to gpt4o

--- invoice_data_extractor.py
+++ invoice_data_extractor.py
@@ -129,7 +129,8 @@ class InvoiceDataExtractor:
 
                 # Call the API
                 response = await self.client.chat.completions.create(
-                    model="gpt-5.4-mini", messages=messages, max_completion_tokens=1000
+                    # model="gpt-5.4-mini", messages=messages, max_completion_tokens=1000
+                    model="gpt-4o", messages=messages, max_completion_tokens=1000
                 )
 
                 response_text = response.choices[0].message.content
